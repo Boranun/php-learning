a7b20314bedee27f8168d3ff0b476a4bbf048c33
deruta <yuuuuyan.0331@gmail.com>
2026-01-31 18:21:46 +0900

    Add: 配列・繰り返し（foreach）・条件分岐（if）

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php
-echo "Hello, PHP Learning!";
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP学習</title>
+</head>
+<body>
+    <hr>
+    <h2>配列と繰り返し</h2>
+    <?php
+    $skills=["React","Next.js","TypeScript","PHP"];
+    echo "<h3>習得済みスキル:</h3>";
+    echo "<ul?>";
+
+    foreach ($skills as $skill){
+        echo "<li>".$skill."</li>";
+    }
+
+    echo "</ul>";
+    ?>
+
+    <hr>
+    <h2>条件分岐</h2>
+    <?php
+    $score = 85;
+    if ($score >=80){
+        echo "<p>優秀です!</p>";
+    }
+    elseif($score>=60){
+        echo "<p>合格です。</p>";
+    }
+    else {
+        echo "<p>もう少し頑張りましょう。</p>";
+    }
+        ?>
+</body>
+</html>
